ae3db02d1ab20078d2fa2694fd733bdafab23b08
Léo Jacquet <leojacquet2003@gmail.com>
2023-04-05 21:00:42 +0200

    continue Navbar

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,18 +1,73 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { iconCart, iconDelete } from '../../assets/icons';
+import imageProduct1 from '../../assets/image-product-1-thumbnail.jpg';
 import './Cart.scss';
 
 const Cart: FC = () => {
+    const [open, setOpen] = useState<boolean>(false);
+    const rootRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (rootRef.current && !rootRef.current.contains(e.target as Node)) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [rootRef]);
+
+    const handleClick = (): void => {
+        setOpen(!open);
+    };
+
+    // TODO: temporaire
+    const [nbProducts, setNbProcust] = useState<number>(3);
+
     return (
-        <div className="cart">
-            <div className="cart-icon" data-nbProduct="3">
-                <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
-                        fill="#69707D"
-                        fill-rule="nonzero"
-                    />
-                </svg>
+        <div className="cart" ref={rootRef}>
+            <div
+                className={'cart-icon' + (nbProducts > 0 ? ' products' : '')}
+                data-nbproducts={nbProducts}
+                onClick={handleClick}>
+                {iconCart}
             </div>
+
+            {open && (
+                <div className="cartPopUp">
+                    <div className="top">
+                        <span>Cart</span>
+                    </div>
+                    <div className="bottom">
+                        {nbProducts > 0 ? (
+                            <>
+                                <div className="product">
+                                    <div className="image">
+                                        <img src={imageProduct1} alt="Product 1" />
+                                    </div>
+
+                                    <div className="text">
+                                        <span className="name">Fall Limited Edition Sneakers</span>
+                                        <span className="price">
+                                            $125.00 x {nbProducts} <b>${(125.00 * nbProducts).toFixed(2)}</b>
+                                        </span>
+                                    </div>
+
+                                    <div className="delete">{iconDelete}</div>
+                                </div>
+
+                                <button className="checkout">Checkout</button>
+                            </>
+                        ) : (
+                            <span className="empty">Your cart is emptys</span>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
